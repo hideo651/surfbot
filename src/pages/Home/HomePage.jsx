0f8c48b16f4a152da9799228595b762bfcadea21
hideo651 <hideo651@msn.com>
2024-09-10 10:51:19 -0300

    bug arrumado

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,7 +1,7 @@
 import Aulas from "../../components/Aula/Aulas";
 import Capa from "../../components/Capa/Capa";
 import Contato from "../../components/Contato/Contato";
-import Conteudo from "../../components/Conteudo/conteudo";
+import Conteudo from "../../components/Conteudo/Conteudo";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 
